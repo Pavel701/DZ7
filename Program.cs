6731ce69792c2d2f8c60bc5ecf6ddaf63fed433e
flip701 <paulshipspb@gmail.com>
2023-05-02 11:32:24 +0300

    Внесены условия задачи

--- Program.cs
+++ Program.cs
@@ -1,59 +1,63 @@
-﻿// class Program {
-//     static void Main() {
-//         int m = 3; // количество строк массива
-//         int n = 4; // количество столбцов массива
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+class Program {
+    static void Main() {
+        int m = 3; // количество строк массива
+        int n = 4; // количество столбцов массива
         
-//         double[,] array = new double[m, n]; // создаем пустой двумерный массив заданного размера
+        double[,] array = new double[m, n]; // создаем пустой двумерный массив заданного размера
 
-//         Random rand = new Random(); // создаем генератор случайных чисел
+        Random rand = new Random(); // создаем генератор случайных чисел
 
-//         // заполняем массив случайными вещественными числами
-//         for (int i = 0; i < m; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 array[i, j] = rand.NextDouble();
-//             }
-//         }
+        // заполняем массив случайными вещественными числами
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i, j] = rand.NextDouble();
+            }
+        }
 
-//         // выводим массив на экран
-//         for (int i = 0; i < m; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 Console.Write(array[i, j] + " ");
-//             }
-//             Console.WriteLine();
-//         }
-//     }
-// }
+        // выводим массив на экран
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
 
-
-// class Program {
-//     static void Main() {
-//         // размер массива
-//         int m = 4;
-//         int n = 5;
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+class Program {
+    static void Main() {
+        // размер массива
+        int m = 4;
+        int n = 5;
         
-//         int[,] matrix = new int[m, n]; // создание двумерного массива
+        int[,] matrix = new int[m, n]; // создание двумерного массива
 
-//         // заполнение массива
-//         for (int i = 0; i < m; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 matrix[i, j] = i * n + j;
-//             }
-//         }
+        // заполнение массива
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i, j] = i * n + j;
+            }
+        }
 
-//         Console.WriteLine("Введите номер строки:"); // чтение позиций элементов с консоли
-//         int row = int.Parse(Console.ReadLine());
-//         Console.WriteLine("Введите номер столбца:");
-//         int col = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите номер строки:"); // чтение позиций элементов с консоли
+        int row = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите номер столбца:");
+        int col = int.Parse(Console.ReadLine());
 
-//         // проверка наличия элемента в массиве и его вывод
-//         if (row >= 0 && row < m && col >= 0 && col < n) {
-//             Console.WriteLine($"Значение элемента [{row}, {col}]: {matrix[row, col]}");
-//         } else {
-//             Console.WriteLine("Такого числа в массиве нет!");
-//         }
-//     }
-// }
+        // проверка наличия элемента в массиве и его вывод
+        if (row >= 0 && row < m && col >= 0 && col < n) {
+            Console.WriteLine($"Значение элемента [{row}, {col}]: {matrix[row, col]}");
+        } else {
+            Console.WriteLine("Такого числа в массиве нет!");
+        }
+    }
+}
 
+
+// № 3 Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 class Program {
     static void Main() {
         // размер массива
