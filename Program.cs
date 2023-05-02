7a441233ef611d7e7285978455875361316c7489
flip701 <paulshipspb@gmail.com>
2023-05-02 10:57:31 +0300

    Решение первой задачи

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿class Program {
+    static void Main() {
+        // размер массива
+        int m = 3;
+        int n = 4;
+
+        // создание двумерного массива
+        double[,] matrix = new double[m, n];
+
+        // заполнение массива случайными вещественными числами
+        Random random = new Random();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i, j] = random.NextDouble();
+            }
+        }
+
+        // вывод массива на консоль
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
